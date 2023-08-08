20f51d778dd21c66953190d0032e9f18aeaeabde
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2023-08-08 09:09:28 -0300

    Project uploaded

---
+++ Assets/Scripts/VehicleScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleScript : MonoBehaviour
+{
+    public float maxFuelCapacity;
+    public float currentFuelCharge;
+    public GameObject gasCanIcon;
+
+}
